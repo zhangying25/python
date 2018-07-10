8ce157f1874955eeb0ab54f7b668ee451f6b89a7
Ying Zhang <zhang_ying@msn.com>
2018-07-10 12:32:37 -0700

    zigzag: handle corner case

--- zigzag.py
+++ zigzag.py
@@ -5,16 +5,21 @@ class Solution(object):
         :type numRows: int
         :rtype: str
         """
+        if numRows < 2 or len(s) <= numRows:
+            return s
+
+        result = ""
         seg = 2 * numRows - 2;
         inc = seg
         for row in range(numRows):
             i = row
             while i < len(s):
-                print s[i],
+                result += s[i]
                 if inc < seg and inc > 0 and i + inc < len(s):
-                    print s[i + inc],
+                    result += s[i + inc]
                 i += seg
             inc -= 2
+        return result
 
 test = Solution()
-test.convert("PAYPALISHIRING", 4)
+print test.convert("PAYPALISHIRING", 1)
